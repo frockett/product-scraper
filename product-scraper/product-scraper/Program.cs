3ff972c2ac20c3f46e2f95dce73b468bce7f06a4
frockett <crockett.d.ford@gmail.com>
2024-04-07 13:54:15 +0800

    playwright is set up

--- product-scraper/product-scraper/Program.cs
+++ product-scraper/product-scraper/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Playwright;
+using System.Formats.Asn1;
+
+var playwright = await Playwright.CreateAsync();
+var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 50 });
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://jp.mercari.com");
+
+// Perform your scraping actions here
+var title = await page.TitleAsync();
+await page.ScreenshotAsync(new()
+{
+    Path = "screenshot.png"
+});
+Console.WriteLine($"Page Title: {title}");
+
+// Clean up
+await browser.CloseAsync();
